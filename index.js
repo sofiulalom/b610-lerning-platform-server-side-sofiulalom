0feb44a7224413e878eab92431a28d4c287427d9
sofiulaloe <sahasofiulalom3@gmail.com>
2023-01-01 16:50:28 +0600

    add server all add commit

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express=require('express');
 const app=express();
-const port= 5500;
+const port=process.env.PORT || 5500;
+const cors=require('cors');
+app.use(cors())
+const catagoris =require('./fackdata/catagory.json')
+const courses =require('./fackdata/course.json')
+
 
 app.get('/', (req, res)=>{
      res.send('hello assignment !!!')
 });
-
+app.get('/catagoris', (req, res)=>{
+    res.send(catagoris)
+})
+app.get('/catagory/:id', (req, res)=>{
+    const id =req.params.id;
+    const select_Catagory=catagoris.filter(catagory => catagory.id ===id )
+    res.send(select_Catagory)
+})
+app.get('/courses', (req, res)=>{
+    res.send(courses)
+})
+app.get('/course/:id', (req, res)=>{
+    const id =req.params.id;
+    const selectedCourses =courses.find(course => course._id === id)
+    res.send(selectedCourses)
+})
 app.listen(port, ()=>{
     console.log(`Example app listening on port ${port}`);
 })
